c91dd970d5309ac693f2d074a8860fa7b8e79696
Kannan R <kannanr@techconative.com>
2022-05-04 13:39:51 +0530

    WIP integration test changes Added test to start jsonbox container and try to run test against it.

---
+++ src/test/java/restel/integration/IntegrationTests.java
@@ -0,0 +1,53 @@
+package restel.integration;
+
+import com.techconative.restel.core.RestelApplication;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+import java.util.Map;
+
+@Testcontainers
+public class IntegrationTests {
+
+  private Network network = Network.newNetwork();
+
+  private GenericContainer mongo =
+      new GenericContainer(DockerImageName.parse("mongo"))
+          .withNetwork(network)
+          .withNetworkAliases("mongo")
+          .withEnv(Map.of("MONGODB_URI", "mongodb://mongo:27017/jsonbox-io-dev"))
+          .withExposedPorts(27017);
+
+  private GenericContainer jsonBox =
+      new GenericContainer(DockerImageName.parse("jsonbox_jsonbox"))
+              .dependsOn(mongo)
+          .withNetwork(network)
+          .withExposedPorts(3000);
+
+  @BeforeEach
+  void setUp() {
+    mongo.start();
+    jsonBox.setPortBindings(List.of("3000","3000"));
+    jsonBox.start();
+  }
+
+  @AfterEach
+  void tearDown() {
+    mongo.close();
+    jsonBox.close();
+    network.close();
+  }
+
+  @Test
+  void testUseCases() {
+    System.setProperty(
+        "app.excelFile", "/Users/kannanr/Desktop/projects/Restel/quickstart/jsonbox_test.xlsx");
+    RestelApplication.main(null);
+  }
+}
