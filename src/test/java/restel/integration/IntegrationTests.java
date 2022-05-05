5966ca2a1ad17e6659eb00114ba088979a7f8814
Kannan R <kannanr@techconative.com>
2022-05-05 22:56:39 +0530

    spotless

--- src/test/java/restel/integration/IntegrationTests.java
+++ src/test/java/restel/integration/IntegrationTests.java
@@ -1,6 +1,5 @@
 package restel.integration;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.techconative.restel.core.RestelApplication;
